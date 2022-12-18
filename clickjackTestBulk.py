0282faaab68524b15edf57cb95a4da70ea062ba4
Faris <36856116+fariskonnakkadan@users.noreply.github.com>
2022-12-18 18:15:26 +0530

    Update clickjackTestBulk.py

--- clickjackTestBulk.py
+++ clickjackTestBulk.py
@@ -2,7 +2,7 @@ import sys
 import re
 
 if len(sys.argv) < 2:
-    print("Error: No text file provided. Please provide a text file with a list of URLs as an argument.\n Usage: clickjackTestBulk.py urls.txt \n urls.txt should contain valid target URLs.")
+    print("Error: No text file provided. Please provide a text file with a list of URLs as an argument.\n Usage: clickjackTestBulk.py urls.txt \n urls.txt should contain valid URLs in textfile.")
     sys.exit()
 
 
@@ -23,5 +23,6 @@ for url in urls:
     # Create the HTML snippet
     html = f"<iframe src='{url}'></iframe>"
     # Output the HTML snippet to a separate file
-    with open(f"{url.split('.')[1]}.html", 'w') as file:
+    url = re.sub(r'[^\w\s]', '', url)
+    with open(f"{url}.html", 'w') as file:
         file.write(html)
